0a004c890b1838fac08acd4b246521fd9b51e9b4
muokieutychus@gmail.com <119785560+icarus-19@users.noreply.github.com>
2026-08-27 15:15:07 +0300

    v1.5.6

--- pokoreze-portfolio/src/App.js
+++ pokoreze-portfolio/src/App.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import SFW from "./pages/SFW";
-import NSFW from "./pages/NSFW";
-import Commissions from "./pages/Commissions";
-import Contact from "./pages/Contact";
+import Home from "./pages/Home.js";
+import SFW from "./pages/SFW.js";
+import NSFW from "./pages/NSFW.js";
+import Commissions from "./pages/Commissions.js";
+import Contact from "./pages/Contact.js";
 import "./App.css";
 
 function App() {
