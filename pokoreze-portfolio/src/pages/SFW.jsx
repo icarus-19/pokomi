88eaa9ae6e888a4c31ef1c5f04a42c7eb0f453aa
muokieutychus@gmail.com <119785560+icarus-19@users.noreply.github.com>
2026-08-27 15:48:36 +0300

    nigga

--- pokoreze-portfolio/src/pages/SFW.jsx
+++ pokoreze-portfolio/src/pages/SFW.jsx
@@ -1,5 +1,5 @@
 function SFW({ darkMode }) {
-  const images = [1,2,3,4,5,6,7,8,9,10,11,,13,14,15,16,17,18,19,20];
+  const images = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
 
   return (
     <section className="gallery">
